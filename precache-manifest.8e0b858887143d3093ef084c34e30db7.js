88e5f15889986a68af80bee5f29718dd46003366
Francisco Bento <franciscobdsjunior@gmail.com>
2020-03-29 20:45:57 -0300

    Updates

---
+++ precache-manifest.8e0b858887143d3093ef084c34e30db7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fb64c4d380aa4b8f195ee199ddbef77",
+    "url": "/tools/index.html"
+  },
+  {
+    "revision": "f9ef9d8af02e8f045090",
+    "url": "/tools/static/css/main.d12e2ab2.chunk.css"
+  },
+  {
+    "revision": "146a1d7e9c62ab882e8c",
+    "url": "/tools/static/js/2.11343563.chunk.js"
+  },
+  {
+    "revision": "0871cfbda4caebc7116d381c3f7a7b73",
+    "url": "/tools/static/js/2.11343563.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9ef9d8af02e8f045090",
+    "url": "/tools/static/js/main.5ee000e2.chunk.js"
+  },
+  {
+    "revision": "da40d36d42851565decb",
+    "url": "/tools/static/js/runtime-main.92c04f79.js"
+  },
+  {
+    "revision": "0fb68d664552ea5f7442e0102b709902",
+    "url": "/tools/static/media/logo.0fb68d66.svg"
+  }
+]);
