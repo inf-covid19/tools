495d296b5810c049267558e002e5b12ed8b1cc61
Felipe Pontes <felipemfpontes@gmail.com>
2020-04-22 22:32:46 -0300

    Fix math median function

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,6 +1,5 @@
 export const median = (raw: number[]) => {
   const arr = [...raw].sort();
-  const mid = Math.floor(arr.length / 2),
-    nums = [...arr].sort((a, b) => a - b);
-  return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
+  const mid = Math.floor(arr.length / 2);
+  return arr.length % 2 !== 0 ? arr[mid] : (arr[mid - 1] + arr[mid]) / 2;
 };
