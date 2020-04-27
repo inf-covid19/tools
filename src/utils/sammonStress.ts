92a0bcd070c6edf9967c74234aa3334003ee1b23
Francisco Bento <franciscobdsjunior@gmail.com>
2020-04-27 11:57:08 -0300

    Add normalized Sammon's stress to projection chart

--- src/utils/sammonStress.ts
+++ src/utils/sammonStress.ts
@@ -1,33 +1,65 @@
-export const getSammonStress = (rawData: any[], reductedData: any[]) => {
+const normFn = (val: number, min: number, max: number) => (val - min) / (max - min);
+
+const normalize = (matrix: Array<number[]>) => {
+  console.log("matrix", matrix);
+
+  let maxValue = matrix[0][0];
+  let minValue = matrix[0][0];
+
+  for (let i = 0; i < matrix.length; i++) {
+    maxValue = Math.max(...matrix[i], maxValue);
+    minValue = Math.min(...matrix[i], minValue);
+  }
+
+  const normalizedMatrix = matrix.map((row) => {
+    return row.map((value) => normFn(value, minValue, maxValue));
+  });
+
+  console.log("normalizedMatrix", normalizedMatrix);
+  return normalizedMatrix;
+};
+
+export const getSammonStress = (rawData: Array<number[]>, reductedData: Array<number[]>) => {
   let dx = 0;
   let dy = 0;
 
-  let topSammon = 0;
-  let bottomSammon = 0;
+  let first = 0;
+  let second = 0;
 
-  for (let i = 0; i < rawData.length; i++) {
-    for (let j = 0; j < rawData.length; j++) {
-      const a = rawData[i];
-      const b = rawData[j];
+  const raw = normalize(rawData);
+  const red = normalize(reductedData);
+  // const raw = rawData;
+  // const red = reductedData;
+
+  console.log("nD");
+  console.log(raw);
+  console.log("2D");
+  console.log(red);
+
+  for (let i = 0; i < raw.length - 1; i++) {
+    for (let j = i + 1; j < raw.length; j++) {
+      const a = raw[i];
+      const b = raw[j];
+
       let rawDist = 0;
-
       for (let d = 0; d < a.length; d++) {
         rawDist += Math.pow(a[d] - b[d], 2);
       }
       dx = Math.sqrt(rawDist);
 
-      const x = reductedData[i];
-      const y = reductedData[j];
+      const x = red[i];
+      const y = red[j];
+
       let reductedDist = 0;
       for (let d = 0; d < x.length; d++) {
         reductedDist += Math.pow(x[d] - y[d], 2);
       }
       dy = Math.sqrt(reductedDist);
 
-      topSammon += Math.pow(dx - dy, 2);
-      bottomSammon += Math.pow(dx, 2);
+      first += dx;
+      second += Math.pow(dx - dy, 2) / dx;
     }
   }
 
-  return topSammon / bottomSammon;
+  return (1 / first) * second;
 };
