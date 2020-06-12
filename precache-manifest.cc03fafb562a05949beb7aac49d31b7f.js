e32bff01a089aacbaeeeae127557b21857f2c44b
GitHub Action <action@github.com>
2020-06-12 18:14:55 +0000

    Updates

---
+++ precache-manifest.cc03fafb562a05949beb7aac49d31b7f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e1e4d8ff3a985154dfdb578e684df7e",
+    "url": "https://covid19.ufrgs.dev/tools/index.html"
+  },
+  {
+    "revision": "f3474ca06da31df2596b",
+    "url": "https://covid19.ufrgs.dev/tools/static/css/main.c340c77b.chunk.css"
+  },
+  {
+    "revision": "43575cdaeadc6cd2d5a3",
+    "url": "https://covid19.ufrgs.dev/tools/static/js/2.bab1f80d.chunk.js"
+  },
+  {
+    "revision": "f5f62dcc226db9a322c3a05fe86effe2",
+    "url": "https://covid19.ufrgs.dev/tools/static/js/2.bab1f80d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3474ca06da31df2596b",
+    "url": "https://covid19.ufrgs.dev/tools/static/js/main.63038923.chunk.js"
+  },
+  {
+    "revision": "a8a5aa730d099ce1600a",
+    "url": "https://covid19.ufrgs.dev/tools/static/js/runtime-main.02811f64.js"
+  },
+  {
+    "revision": "0fb68d664552ea5f7442e0102b709902",
+    "url": "https://covid19.ufrgs.dev/tools/static/media/logo.0fb68d66.svg"
+  },
+  {
+    "revision": "1abd8f9e244e530d3a1ab0c922b4a613",
+    "url": "https://covid19.ufrgs.dev/tools/static/media/ufrgs-inf.1abd8f9e.png"
+  },
+  {
+    "revision": "6440c8a13158bb4b7d2155d90ff2ae99",
+    "url": "https://covid19.ufrgs.dev/tools/static/media/ufrgs.6440c8a1.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://covid19.ufrgs.dev/tools/precache-manifest.a3a82b89e5fb2771b783098854cb95fb.js"
+  "https://covid19.ufrgs.dev/tools/precache-manifest.cc03fafb562a05949beb7aac49d31b7f.js"
 );
 
 self.addEventListener('message', (event) => {
